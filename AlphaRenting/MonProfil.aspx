620b89b73d5f666600f51b89d5e9de9bf2377293
Chavret <glorian.geoffrey@gmail.com>
2016-03-15 16:39:26 +0100

    Connexion BDD et monprofil ca marche

---
+++ AlphaRenting/MonProfil.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MonProfil.aspx.cs" Inherits="AlphaRenting.MonProfil" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:FormView runat="server" ID="modifprofil" DefaultMode="Edit">
+            <InsertItemTemplate>
+                <table>                   
+                    <tr><td>Choisissez votre secteur : </td>
+                    <td>
+                        <asp:DropDownList runat="server" ID="Listederoulante" >
+                            <asp:ListItem Selected="true" Value="Comedien">Comedien</asp:ListItem>
+                            <asp:ListItem Value="Magicien">Magicien</asp:ListItem>
+                            <asp:ListItem Value="Hotesse">Hotesse</asp:ListItem>
+                        </asp:DropDownList></td>
+                    </tr>
+                    <tr><td>Age : </td>
+                        <td><asp:TextBox runat="server" ID="age" ToolTip="age" /></td>
+                    </tr>
+                    <tr><td>Adresse mail : </td>
+                        <td><asp:TextBox runat="server" ID="mail" ToolTip="adresse mail" /></td>
+                    </tr>
+                    <tr><td>Departement : </td>
+                    <td><asp:TextBox runat="server" ID="dep" ToolTip="Departement" /></td>
+                    </tr>
+                    <tr>
+                        <td> CV : <asp:FileUpload runat="server" ID="CV" /></br>
+                            <asp:Label runat="server" ID="LabelCV" Text="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Photo : <asp:FileUpload runat="server" ID="photo" AllowMultiple="true" /></br>
+                            <asp:Label runat="server" ID="LabelPhoto" Text="" /></td>
+                    </tr>
+                    <tr>
+                        <td>Video : <asp:FileUpload runat="server" ID="video" /></br>
+                            <asp:Label runat="server" ID="LabelVideo" Text="" /></td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <asp:Button runat="server" ID="modifier" OnClick="modifier_Click" Text="Modifier Profil" /></td>
+                    </tr>
+                </table>
+            </InsertItemTemplate>
+        </asp:FormView>
+    </form>
+</body>
+</html>
---
+++ AlphaRenting/MonProfil.aspx.cs
@@ -0,0 +1,170 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AlphaRenting
+{
+    public partial class MonProfil : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void modifier_Click(object sender, EventArgs e)
+        {
+            bool connecter = false;
+            if (Session["adresse_mail"] != null)
+            {
+                connecter = true;
+            }
+            if (connecter == true) { 
+                string CVfilename = null, photofilename = null, videofilename = null;
+                string metier = null, mail = null;
+                int age = 0, departement=0;
+                DropDownList dd = (DropDownList)modifprofil.FindControl("Listederoulante");
+                metier = dd.SelectedValue;
+                if (!string.IsNullOrWhiteSpace(metier))
+                    DB.Update("comedien", "secteur", "adresse_mail", metier, Session["adresse_mail"]);
+                TextBox tb = (TextBox)modifprofil.FindControl("dep");
+                if (!string.IsNullOrWhiteSpace(tb.Text))
+                {
+                    departement = Convert.ToInt32(tb.Text);
+                    DB.Update("comedien", "departement", "adresse_mail", departement, Session["adresse_mail"]);
+                }
+                tb = (TextBox)modifprofil.FindControl("mail");
+                if (!string.IsNullOrWhiteSpace(tb.Text))
+                {
+                    mail = tb.Text;
+                    DB.Update("comedien", "adresse_mail", "adresse_mail", mail, Session["adresse_mail"]);
+                    Session["adresse_mail"] = mail;
+                }
+                tb = (TextBox)modifprofil.FindControl("age");
+                if (!string.IsNullOrWhiteSpace(tb.Text))
+                {
+                    age = Convert.ToInt32(tb.Text);
+                    DB.Update("comedien", "age", "adresse_mail", age, Session["adresse_mail"]);
+                }
+                FileUpload fp = (FileUpload)modifprofil.FindControl("CV");
+                Label lb1 = (Label)modifprofil.FindControl("LabelCV");
+                if (fp.HasFile)
+                {
+                    try
+                    {
+                        if (fp.PostedFile.ContentType == "image/jpeg")
+                        {
+                            if (fp.PostedFile.ContentLength < 102400)
+                            {
+                                CVfilename = fp.FileName;
+                                fp.SaveAs(Server.MapPath("~/cv/") + CVfilename);
+                                lb1.Text = "Upload status: File uploaded!";
+                                CVfilename = ("~/cv/" + fp.FileName);
+                                DB.Update("Comedien", "CV", "adress_mail", CVfilename, Session["adresse_mail"]);
+                            }
+                            else {
+                                lb1.Text = "Upload status: The file as to be less than 100Kb !";
+                                lb1.Focus();
+                            }
+                        }
+                        else {
+                            lb1.Text = "Upload status: Only jpeg files are accepted !";
+                            lb1.Focus();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lb1.Text = "Upload status: the file could not be uploaded. The following error occured: " + ex.Message;
+                        lb1.Focus();
+                    }
+                }
+                FileUpload fp1 = (FileUpload)modifprofil.FindControl("photo");
+                Label lb2 = (Label)modifprofil.FindControl("LabelPhoto");
+                if (fp1.HasFile)
+                {
+                    string filepath = Server.MapPath("~/photos");
+                    HttpFileCollection uploadedfile = Request.Files;
+                    for (int i = 0; i < uploadedfile.Count; i++)
+                    {
+                        HttpPostedFile userpostedfile = uploadedfile[i];
+                        try
+                        {
+                            if (userpostedfile.ContentType == "image/jpeg")
+                            {
+                                if (userpostedfile.ContentLength < 102400)
+                                {
+                                    photofilename = "~/photos/" + userpostedfile.FileName;
+                                    userpostedfile.SaveAs(Server.MapPath("~/photos/") + userpostedfile.FileName);
+                                    lb2.Text = "Upload status: File uploaded!";
+                                    DB.Insert("photo", "photo_path", photofilename);
+                                    int idcomedien = 0, idphoto = 0;
+                                    MySqlDataReader Dr = DB.Select("comedien", "id", "adresse_mail", Session["adresse_mail"]);
+                                    if (Dr.HasRows) { idcomedien = Convert.ToInt32(Dr.Read()); }
+                                    Dr = DB.Select("photo", "idphoto", "photo_path", photofilename);
+                                    if (Dr.HasRows) { idphoto = Convert.ToInt32(Dr.Read()); }
+                                    DB.Insert("comedien_photo", "idphoto,idcomedien", idphoto, idcomedien);
+                                }
+                                else {
+                                    lb2.Text = "Upload status: The file as to be less than 100Kb !";
+                                    lb2.Focus();
+                                }
+                            }
+                            else {
+                                lb2.Text = "Upload status: Only jpeg files are accepted !";
+                                lb2.Focus();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            lb2.Text = "Upload status: the file could not be uploaded. The following error occured: " + ex.Message;
+                            lb2.Focus();
+                        }
+                    }
+                }
+                FileUpload fp2 = (FileUpload)modifprofil.FindControl("video");
+                Label lb3 = (Label)modifprofil.FindControl("LabelVideo");
+                if (fp2.HasFile)
+                {
+                    try
+                    {
+                        if (fp2.PostedFile.ContentType == "video/MP4")
+                        {
+                            if (fp2.PostedFile.ContentLength < 102400)
+                            {
+                                videofilename = "~/video/" + fp2.FileName;
+                                fp2.SaveAs(Server.MapPath("~/video/") + fp2.FileName);
+                                lb3.Text = "Upload status: File uploaded!";
+                                DB.Insert("video", "video_path", videofilename);
+                                int idcomedien = 0, idvideo = 0;
+                                MySqlDataReader Dr = DB.Select("comedien", "id", "adresse_mail", Session["adresse_mail"]);
+                                if (Dr.HasRows) { idcomedien = Convert.ToInt32(Dr.Read()); }
+                                Dr = DB.Select("video", "idvideo", "video_path", videofilename);
+                                if (Dr.HasRows) { idvideo = Convert.ToInt32(Dr.Read()); }
+                                DB.Insert("comedien_video", "idvideo,idcomedien", idvideo, idcomedien);
+                            }
+                            else {
+                                lb3.Text = "Upload status: The file as to be less than 100Kb !";
+                                lb3.Focus();
+                            }
+                        }
+                        else {
+                            lb3.Text = "Upload status: Only jpeg files are accepted !";
+                            lb3.Focus();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        lb3.Text = "Upload status: the file could not be uploaded. The following error occured: " + ex.Message;
+                        lb3.Focus();
+                    }
+                }
+            }else
+            {
+                Server.Transfer("Connexion.aspx");
+            }
+        }
+    }
+}
